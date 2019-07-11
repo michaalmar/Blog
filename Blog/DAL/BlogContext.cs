340c5a016965c30d4d0b76f9230e86354ef22c07
michaalmar <michalmarchela@gmail.com>
2019-07-11 09:42:50 +0200

    Blog Model

---
+++ Blog/DAL/BlogContext.cs
@@ -0,0 +1,27 @@
+﻿using Blog.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+namespace Blog.DAL
+{
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("BlogContext")
+        {
+
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string AuthorName { get; set; }
+
+        //  public int UserId { get; set; }
+
+
+
+
+    }
+}
